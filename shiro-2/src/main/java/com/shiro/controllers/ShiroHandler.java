cc5e70883bd0b4f981ecf0d0182bb22d795310c6
timefireworks <1158388143@qq.com>
2018-11-19 17:21:33 +0800

    shiro整合spring

---
+++ shiro-2/src/main/java/com/shiro/controllers/ShiroHandler.java
@@ -0,0 +1,37 @@
+package com.shiro.controllers;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * author: San Jinhong
+ * date: 2018/11/19 15:04
+ **/
+@Controller
+@RequestMapping("/shiro")
+public class ShiroHandler {
+
+    @RequestMapping( value = "shiroLogin")
+    public String login(@RequestParam String username,
+                        @RequestParam String password){
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if(!currentUser.isAuthenticated()){
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            token.setRememberMe(true);
+            try {
+                System.err.println("1:" + token.hashCode());
+                currentUser.login(token);
+            } catch (AuthenticationException ae){
+                System.out.println("登录失败" + ae.getMessage());
+            }
+        }
+        return "redirect:/list.jsp";
+    }
+}
